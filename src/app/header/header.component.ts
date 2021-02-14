81cf92bf5283d112124f26eca318be8d5454e24f
Luis <luchopintado@gmail.com>
2021-02-14 15:24:42 -0500

    Fix links on header and recipe detail

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,14 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    templateUrl: 'header.component.html'
+    templateUrl: 'header.component.html',
+    styleUrls: ['header.component.css']
 })
 export class HeaderComponent {
     collapsed = true;
-    @Output() featureSelected = new EventEmitter<string>();
 
     toggleNavbar(): void {
         this.collapsed = !this.collapsed;
     }
-
-    /*onSelect(feature: string): void {
-        this.featureSelected.emit(feature);
-    }*/
 }
